2bcb9a3ecba50b630f16c5543dd09d9901d61b64
kreby4555 <contatocleber822@gmail.com>
2024-04-17 20:14:23 -0300

    Conexão com o MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 /* Conexão dos bancos de dados */
 
 import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
 import User from '../app/models/Users'
 import configDataBase from '../config/database'
 import Product from '../app/models/Product'
@@ -10,11 +11,20 @@ const models = [User, Product, Category] // recebendo os models
 class Database {
   constructor () {
     this.init()
+    this.mongo()
   }
 
   init () { // configuração da conecção dos Models
     this.connection = new Sequelize(configDataBase)
     models.map((model) => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models))
+  }
+
+  mongo () {
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/codeburger',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
   }
 }
 
